b0bfa4204f8c7bc0eb30bee61b81db4212b0bb22
Christian Liebel <christian.liebel@thinktecture.com>
2018-10-14 11:54:08 +0200

    feat(app): add CORS support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Logger, Module } from '@nestjs/common';
+import { CorsMiddleware } from '@nest-middlewares/cors';
+import { Logger, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { TodoService } from './sync/todo.service';
+import { PushController } from './push/push.controller';
 import { PushService } from './push/push.service';
 import { SyncController } from './sync/sync.controller';
-import { PushController } from './push/push.controller';
+import { TodoService } from './sync/todo.service';
 import { StorageService } from './storage.service';
 
 @Module({
@@ -11,4 +12,11 @@ import { StorageService } from './storage.service';
   controllers: [AppController, SyncController, PushController],
   providers: [TodoService, PushService, Logger, StorageService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
